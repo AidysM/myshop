671604f51898bc3f94fcf0284c49aa7844c68add
AidysM <mongush10011979@gmail.com>
2021-04-25 03:48:29 +0700

    added cart

--- myshop/settings.py
+++ myshop/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
-
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -130,4 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+CART_SESSION_ID = 'cart'
 
--- shop/models.py
+++ shop/models.py
@@ -17,6 +17,7 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('shop:product_list_by_category', args=[self.slug])
 
+
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=200, db_index=True)
